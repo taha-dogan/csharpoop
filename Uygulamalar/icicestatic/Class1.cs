16adcc2782dbd0d20b6de8c9eb88e9b88eb75bbc
Taha <71967160+taha93230@users.noreply.github.com>
2021-10-13 11:51:07 +0300

    Add files via upload

---
+++ Uygulamalar/icicestatic/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace icicestatic
+{
+    static class SinifB
+    {
+        public static string AdSoyad = "Taha Doğan";
+        public static void AdGoster()
+        {
+            System.Windows.Forms.MessageBox.Show("SinifB : "+ AdSoyad);
+        }
+
+        public static class SinifC
+        {
+            public static string AdSoyad = "Aslı Doğan";
+            public static void AdGoster()
+            {
+                System.Windows.Forms.MessageBox.Show("SinifC : "+ AdSoyad);
+            }
+        }
+    }
+}
---
+++ Uygulamalar/staticclassicindebaskaclass/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staticclassicindebaskaclass
+{
+    static class SinifB
+    {
+        public static string AdSoyad = "Taha Doğan";
+        public static void AdGoster()
+        {
+            System.Windows.Forms.MessageBox.Show(AdSoyad);
+        }
+
+        public class SinifC //static değil ama hata yok
+        {
+            public string AdSoyad = "Ali Doğan";
+            public void AdGoster()
+            {
+                System.Windows.Forms.MessageBox.Show(AdSoyad);
+            }
+        }
+        
+    }
+}
